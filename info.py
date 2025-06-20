efc470ed2422f184fb379f014c873e903f80dfbe
Dkwtf12 <121540365+Dkwtf12@users.noreply.github.com>
2025-06-20 22:16:47 +0530

    Update info.py

--- info.py
+++ info.py
@@ -64,9 +64,9 @@ LOG_VR_CHANNEL = int(environ.get('LOG_VR_CHANNEL', '-1001588212650')) #Verificat
 LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', '-1001588212650')) #If Anyone Set Your Bot In Any Group And Set Shortner In That Group Then In This Channel The All Details Come
 VERIFY_IMG = environ.get("VERIFY_IMG", "https://telegra.ph/file/9ecc5d6e4df5b83424896.jpg")
 
-TUTORIAL = environ.get("TUTORIAL", "https://t.me/how_to_dwnload/47")
+TUTORIAL = environ.get("TUTORIAL", "https://t.me/how_to_dwnload/42")
 TUTORIAL_2 = environ.get("TUTORIAL_2", "https://t.me/how_to_dwnload/45")
-TUTORIAL_3 = environ.get("TUTORIAL_3", "https://t.me/how_to_dwnload/47")
+TUTORIAL_3 = environ.get("TUTORIAL_3", "https://t.me/how_to_dwnload/42")
 
 # Verification (Must Fill All Veriables. Else You Got Error
 SHORTENER_API = environ.get("SHORTENER_API", "a37d35def762b00679123e4bafab4be0416f43c6")
