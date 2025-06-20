4bd9f70740068ffe606c311c5efdc1b8421f7e08
Dkwtf12 <121540365+Dkwtf12@users.noreply.github.com>
2025-06-20 22:05:35 +0530

    Update info.py

--- info.py
+++ info.py
@@ -75,8 +75,8 @@ SHORTENER_WEBSITE = environ.get("SHORTENER_WEBSITE", "linkcents.com")
 SHORTENER_API2 = environ.get("SHORTENER_API2", "826eb92e893f6fdb42920983bba04c409bdc0b5d")
 SHORTENER_WEBSITE2 = environ.get("SHORTENER_WEBSITE2", "tnshort.net")
 
-SHORTENER_API3 = environ.get("SHORTENER_API3", "cb930d3555c570acaa6b188d4faca5c530b39ef4")
-SHORTENER_WEBSITE3 = environ.get("SHORTENER_WEBSITE3", "linkcents.com")
+SHORTENER_API3 = environ.get("SHORTENER_API3", "a37d35def762b00679123e4bafab4be0416f43c6")
+SHORTENER_WEBSITE3 = environ.get("SHORTENER_WEBSITE3", "shortyfi.site")
 
 TWO_VERIFY_GAP = int(environ.get('TWO_VERIFY_GAP', "120"))
 THREE_VERIFY_GAP = int(environ.get('THREE_VERIFY_GAP', "200"))
