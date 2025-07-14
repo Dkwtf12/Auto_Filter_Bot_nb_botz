7f6ff72f51f1f295bf8cd4c2fa3406a433ffd8fa
Dkwtf12 <121540365+Dkwtf12@users.noreply.github.com>
2025-07-14 21:27:36 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -273,7 +273,7 @@ async def start(client, message):
         except Exception as e:
             await log_error(client, f"Got Error In Verification Funtion.\n\n Error - {e}")
             print(f"Error In Verification - {e}")
-            await message.reply_text(f"Something Want Wrong ! Message Here - @SilentXBotz_Support")
+            await message.reply_text(f"Something Want Wrong ! Message Here - @Request_Corner1")
 
     if data.split("-", 1)[0] == "BATCH":
         sts = await message.reply("<b>Please wait...</b>")
